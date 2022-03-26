a58732a53ba4266144163b35a266262485e3e4dc
César Daniel Pérez Olvera <danielprz.contacto@gmail.com>
2022-03-26 17:07:39 -0600

    Initial commit

---
+++ src/test/java/com/github/danielprz1394/applications/configuration_server/ConfigurationServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.danielprz1394.applications.configuration_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigurationServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
